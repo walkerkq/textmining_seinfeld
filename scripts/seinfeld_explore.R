29cbebd02c786bc412b70f23032ca80c0e56c658
Kaylin Walker <walkerkq2@gmail.com>
2016-10-10 13:41:22 -0500

    Further exploration

--- scripts/seinfeld_explore.R
+++ scripts/seinfeld_explore.R
@@ -1,88 +1,71 @@
 library(ggplot2)
-setwd("/Users/kaylinwalker/R/textmining_seinfeld")
+#setwd("/Users/kaylinwalker/R/textmining_seinfeld")
+setwd("/Users/kwalker/git_projects/textmining_seinfeld")
 
-episodes <- read.csv("data/seinfeld_episodes.csv", stringsAsFactors=F)
-episodes$num <- 1:length(episodes$date)
-episodes$Jerry <- sapply(episodes$writers, function(x) if(grepl("Jerry Seinfeld", x)){ x <- 1} else {x <- 0})
-episodes$Larry <- sapply(episodes$writers, function(x) if(grepl("Larry David", x)){ x <- 1} else {x <- 0})
-for(o in seq_along(episodes$title)){
-     if(episodes$num[o] > 0 & episodes$num[o] <= 5) episodes$season[o] <- 1
-     if(episodes$num[o] > 5 & episodes$num[o] <= 17) episodes$season[o] <- 2
-     if(episodes$num[o] > 17 & episodes$num[o] <= 40) episodes$season[o] <- 3
-     if(episodes$num[o] > 40 & episodes$num[o] <= 64) episodes$season[o] <- 4
-     if(episodes$num[o] > 64 & episodes$num[o] <= 86) episodes$season[o] <- 5
-     if(episodes$num[o] > 86 & episodes$num[o] <= 108) episodes$season[o] <- 6
-     if(episodes$num[o] > 108 & episodes$num[o] <= 132) episodes$season[o] <- 7
-     if(episodes$num[o] > 132 & episodes$num[o] <= 154) episodes$season[o] <- 8
-     if(episodes$num[o] > 155 & episodes$num[o] <= 176) episodes$season[o] <- 9
-}
-
-all.text <- read.csv("data/seinfeld_scripts.csv", stringsAsFactors=F)
-all.text$word.count <- sapply(all.text$words, function(x) length(strsplit(x, " ")[[1]]))
-for(y in seq_along(all.text$episode)){
-     h <- episodes[episodes$title %in% all.text$episode[y], ]
-     all.text$season[y] <- h$season
-     #all.text$Jerry[y] <- h$Jerry
-     #all.text$Larry[y] <- h$Larry
-}
+all.text <- read.csv("data/seinfeld_scripts_enhanced.csv", stringsAsFactors=F)
 all.text$date <- as.Date(all.text$date)
-all.text$Overall.Share <- 0
-all.text$Episode.Share <- 0
-all.text$Season.Share <- 0
-for(j in seq_along(all.text$num)){
-     all.text$Overall.Share[j] <- all.text$word.count[j]/sum(all.text$word.count)
-     all.text$Episode.Share[j] <- round(all.text$word.count[j]/sum(all.text$word.count[all.text$num %in% all.text$num[j]]), 4)
-     all.text$Season.Share[j] <- round(all.text$word.count[j]/sum(all.text$word.count[all.text$season %in% all.text$season[j]]), 4)
-}
 
-# share of words overall
-shareOverall <- aggregate(Overall.Share ~ speaker, all.text, sum)
-other <- data.frame(speaker="OTHER", Overall.Share=sum(shareOverall$Overall.Share[!(shareOverall$speaker %in% c("JERRY", "ELAINE", "GEORGE", "KRAMER"))]))
+############################################################
+########## share of words overall
+shareOverall <- aggregate(word.count ~ speaker, all.text, sum)
+shareOverall$Overall.Share <- sapply(shareOverall$word.count, function(x) x/sum(shareOverall$word.count))
+other <- data.frame(speaker="OTHER", 
+                    word.count=sum(shareOverall$word.count[!(shareOverall$speaker %in% c("JERRY", "ELAINE", "GEORGE", "KRAMER"))]),
+                    Overall.Share=sum(shareOverall$Overall.Share[!(shareOverall$speaker %in% c("JERRY", "ELAINE", "GEORGE", "KRAMER"))]))
 shareOverall <- rbind(shareOverall[shareOverall$speaker %in% c("JERRY", "ELAINE", "GEORGE", "KRAMER"),], other)
-shareOverall$group <- ""
+#write.csv(shareOverall, "seinfeld_plot_shareoveral.csv", row.names=F)
 
-shareOverall$speaker <- factor(as.character(shareOverall$speaker), levels=c("JERRY", "OTHER", "GEORGE", "KRAMER", "ELAINE"))
-shareOverall <- shareOverall[order(shareOverall$speaker), ]
-ggplot(shareOverall, aes(group, Overall.Share*100)) + geom_bar(stat="identity", aes(fill=speaker)) + labs(title="Share of Words Spoken, Overall") +
-     xlab("") + ylab("Share of Words Spoken (%)") + theme_classic() + scale_fill_brewer(palette="Paired") + 
-     geom_text(aes(x=group, y=cumsum(Overall.Share*100)-Overall.Share*50, label=paste(speaker,": ",round(Overall.Share*100,1),"%", sep="")), 
-               color="white", fontface="bold", size=4) +
-     theme(legend.position=1, axis.title.y=element_text(margin=margin(0,10,0,0)))
-# write.csv(shareOverall, "seinfeld_plot_shareoveral.csv", row.names=F)
-
-# share of words spoken by season
-shareBySeason <- aggregate(Season.Share ~ speaker + season, all.text, sum)
-other <- aggregate(Season.Share ~ season, all.text[!(all.text$speaker %in% c("JERRY", "ELAINE", "GEORGE", "KRAMER")),], sum)
-other$speaker <- "OTHER"
-other <- other[,c(3,1,2)]
-seasonPlot <- rbind(other, shareBySeason[shareBySeason$speaker %in% c("JERRY", "ELAINE", "GEORGE", "KRAMER"), ])
-ggplot(seasonPlot, aes(as.character(season), Season.Share*100)) + 
-     geom_bar(stat="identity",  aes(fill=speaker)) + labs(title="Share of Words Spoken by Character by Season") +
-     scale_fill_brewer(palette="Paired") +
-     xlab("Season") + ylab("Share of Words (%)") + facet_grid(.~speaker) + theme_bw()
-# write.csv(seasonPlot, "seinfeld_plot_sharebyseason.csv", row.names=F)
-
-# gender disparity
-gender <- read.csv("data/all.speakers.csv", stringsAsFactors=F)
-for(i in seq_along(all.text$num)){
-     all.text$gender[i] <- gender[gender$Name %in% all.text$speaker[i], 3]
+############################################################
+##########  share of words spoken by season
+shareBySeason <- aggregate(word.count ~ speaker + season, all.text, sum)
+for(j in seq_along(shareBySeason[,1])){
+    season.total <- sum(shareBySeason$word.count[shareBySeason$season %in% shareBySeason$season[j]])
+    shareBySeason$Season.Share[j] <- shareBySeason$word.count[j]/season.total
 }
+other <- aggregate(Season.Share ~ season, shareBySeason[!(shareBySeason$speaker %in% c("JERRY", "ELAINE", "GEORGE", "KRAMER")),  ], sum)
+other$speaker <- "OTHER"
+other$word.count <- aggregate(word.count ~ season, shareBySeason[!(shareBySeason$speaker %in% c("JERRY", "ELAINE", "GEORGE", "KRAMER")),  ], sum)[,2]
+shareBySeason <- rbind(shareBySeason[shareBySeason$speaker %in% c("JERRY", "ELAINE", "GEORGE", "KRAMER"),], other[,c(3,1,4,2)])
+#write.csv(shareBySeason, "seinfeld_plot_sharebyseason.csv", row.names=F)
+
+############################################################
+##########  gender disparity
 genderByEp <- aggregate(word.count ~ gender + episode + num, all.text[all.text$gender %in% c("F", "M"), ], sum)
 genderByEp$Share <- 0
 for(j in seq_along(genderByEp$gender)){
      genderByEp$Share[j] <- genderByEp$word.count[j] / sum(genderByEp$word.count[genderByEp$episode %in% genderByEp$episode[j]])
 }
-ggplot(genderByEp, aes(num, Share*100)) + geom_bar(stat="identity", aes(fill=gender)) +
-     xlab("Episode #") + ylab("") + labs(title="Share of Words Spoken by Female Characters") +
-     theme_bw() + scale_fill_brewer(palette="Paired") + theme(legend.position=1) + coord_flip()
-# write.csv(genderByEp, "seinfeld_plot_sharebyep.csv", row.names=F)
-
-all.text$group <- "-"
-ggplot(all.text[all.text$gender %in% c("F", "M"), ], aes(group, word.count)) + 
-     geom_bar(stat="identity", aes(fill=gender)) + coord_flip() + facet_grid(season ~ .)
+genderExp <- dcast(genderByEp, num ~ gender)
+# write.csv(genderExp, "seinfeld_plot_sharebyep.csv", row.names=F)
 
 
+############################################################
+#all.text$group <- "-"
+#ggplot(all.text, aes(group, word.count)) + geom_bar(stat="identity", aes(fill=Larry)) + 
+#    coord_flip() + facet_grid(season ~ .) + scale_fill_brewer(palette="Paired")
 
-speaker <- read.csv("seinfeld_tdm_speaker.csv", stringsAsFactors=F)
-ep <- read.csv("seinfeld_tdm_episode.csv", stringsAsFactors=F)
+# does Jerry talk more in episodes he wrote? NO
+j <- aggregate(word.count ~ num + speaker, all.text[all.text$speaker %in% "JERRY", ], sum)
+all <- aggregate(word.count ~ num, all.text, sum)    
+j <- merge(j, all, by="num", all.x=TRUE)
+j$Share <- j$word.count.x/j$word.count.y
+larry <- unique(all.text[ , c(4,6,7,8,9,14,15)])
+j <- merge(j, larry[,c(1,6)], by="num", all.x=TRUE)
+t.test(Share ~ Jerry, j) # not really significant
+ggplot(j, aes(Jerry, Share*100, group=Jerry)) + geom_boxplot() + labs(title="Does Jerry Talk More in Episodes He Wrote?") +
+    xlab("Episode Written by Jerry Seinfeld") + ylab("Share of Words Spoken (%)")
+
+# does George talk more in episodes Larry David wrote? YES
+g <- aggregate(word.count ~ num + speaker, all.text[all.text$speaker %in% "GEORGE", ], sum)
+g <- merge(g, all, by="num", all.x=TRUE)
+g$Share <- g$word.count.x/g$word.count.y
+g <- merge(g, larry[,c(1,7)], by="num", all.x=TRUE)
+t.test(Share ~ Larry, g) # significant
+ggplot(g, aes(Larry, Share*100, group=Larry)) + geom_boxplot() + labs(title="Does George Talk More in Episodes Larry David Wrote?") +
+    xlab("Episode Written by Larry David") + ylab("Share of Words Spoken (%)")
+
+
+
+speaker <- read.csv("data/seinfeld_tdm_speaker.csv", stringsAsFactors=F)
+ep <- read.csv("data/seinfeld_tdm_episode.csv", stringsAsFactors=F)
 
--- scripts/seinfeld_scrape.R
+++ scripts/seinfeld_scrape.R
@@ -1,5 +1,6 @@
 library(XML)
 library(RCurl)
+setwd("/Users/kwalker/git_projects/textmining_seinfeld")
 
 URL <- "http://www.imsdb.com/TV/Seinfeld.html"
 html <- htmlTreeParse(getURL(URL, followlocation=TRUE), useInternal=TRUE)
@@ -57,3 +58,70 @@ for(h in seq_along(episodes$link)) {
      all.text <- rbind(all.text, lines)
 }
 #write.csv(all.text, "seinfeld_scripts.csv", row.names=F)
+
+######### enhance scripts with other data
+### EPISODE DATA
+episodes <- read.csv("data/seinfeld_episodes.csv", stringsAsFactors=F)
+episodes$num <- 1:length(episodes$date)
+episodes$Jerry <- sapply(episodes$writers, function(x) if(grepl("Jerry Seinfeld", x)){ x <- TRUE } else {x <- FALSE })
+episodes$Larry <- sapply(episodes$writers, function(x) if(grepl("Larry David", x)){ x <- TRUE } else {x <- FALSE })
+for(o in seq_along(episodes$title)){
+    if(episodes$num[o] > 0 & episodes$num[o] <= 5) episodes$season[o] <- 1
+    if(episodes$num[o] > 5 & episodes$num[o] <= 17) episodes$season[o] <- 2
+    if(episodes$num[o] > 17 & episodes$num[o] <= 40) episodes$season[o] <- 3
+    if(episodes$num[o] > 40 & episodes$num[o] <= 64) episodes$season[o] <- 4
+    if(episodes$num[o] > 64 & episodes$num[o] <= 86) episodes$season[o] <- 5
+    if(episodes$num[o] > 86 & episodes$num[o] <= 108) episodes$season[o] <- 6
+    if(episodes$num[o] > 108 & episodes$num[o] <= 132) episodes$season[o] <- 7
+    if(episodes$num[o] > 132 & episodes$num[o] <= 154) episodes$season[o] <- 8
+    if(episodes$num[o] > 155 & episodes$num[o] <= 176) episodes$season[o] <- 9
+}
+### GIRLFRIENDS
+#http://fusion.net/story/155370/heres-what-happened-to-every-single-woman-jerry-dated-on-seinfeld/
+girls <- read.csv("data/seinfeld_girlfriends_seinology.csv", stringsAsFactors=F)
+girls <- unique(toupper(girls$The.Girl))
+girls <- c(girls, "VANESSA", "MARY", "SHERRY", "ISABEL", "GINA", "NINA", "GAIL", "LAURA", "JODI", "PAULA",
+           "KAREN", "MARGARET", "KATYA", "LOIS", "CATHY", "SHELLY", "BRIDGETTE", "GENNICE", "SHEILA", 
+           "HALLIE", "PAM", "ABBY", "VALERIE", "MELISSA", "GWEN", "KERI", "SOPHIE", "LISI", "CINDY" )
+girls <- girls[girls != "ELAINE"]
+
+georges_girls <- c('MARLENE', 'PATRICE', 'MONICA', 'AUDREY', 'EVIE', 'NOELLE', 'CYNTHIA', 'CARRIE', 'ROBIN',
+                   'SUSAN', 'CHERYL', 'ALISON', 'BETSY', 'KAREN', 'GWEN', 'SASHA', 'DIANE', 'DAPHNE', 'ROBBIN',
+                   'JANE', 'VICTORIA', 'JULIE', 'LINDSEY', 'NINA', 'BONNIE', 'DENISE', 'PAULA', 'NANCY', 'SIENNA',
+                   'MARISA', 'ANNA', 'SHEILA', 'HEATHER', 'LOUISE', 'CELIA', 'ALLISON', 'DANIELLE', 'MARCY',
+                   'MARYANN', 'TARA', 'VIVIAN', 'RHISA', 'MIRANDA', 'JANET', 'MAURA')
+elaines_bfs <- c("ROBERT", 'OWEN', 'DICK', 'KEITH', 'EDUARDO', 'DR. RESTON', 'DAVOLA', 'DOCTOR',
+                 'JOHN F. KENNEDY JR.', 'FRED', 'ROY', 'CARL', 'RUSSELL', 'LLOYD', 'JOEL', 'RICKY', 
+                 'TONY', 'PHIL', 'AARON', 'JAKE', 'SIMON', 'NED', 'WHATLEY', 'ROBERT', 'JIMMY', 'PUDDY', 
+                 'BOB', 'JAMES', 'FRED', 'BILLY', 'JOHN', 'BOB', 'CRAIG', 'TODD', 'DAVID', 'KEVIN',
+                 'BRETT', 'BEN', 'ALAN', 'KURT', 'VINCENT', 'BLAINE', 'HAL', 'MR. LIPPMAN', 'JACK',
+                 'PETER', 'GLENN', 'DARRYL', 'ZACH')
+kramers_girls <- c("TINA", 'MARION', 'CHELSEA', 'GAIL', 'LOLA', 'LESLIE', 'CHERYL', 'ERICA', 'OLIVE', 'TOBY',
+                   'NOREEN', 'WENDY', 'SALLY', 'CONNIE', 'PAM', 'EMILY')
+### GENDER
+gender <- read.csv("data/all.speakers.csv", stringsAsFactors=F)
+all.text <- read.csv("data/seinfeld_scripts.csv", stringsAsFactors=F)
+all.text$words <- gsub("\\s*\\([^\\)]+\\)", "", all.text$words) # remove items in parentheses
+all.text$word.count <- sapply(all.text$words, function(x) length(strsplit(x, " ")[[1]])) # word count
+all.text$jerry_gf <- FALSE
+all.text$george_gf <- FALSE
+all.text$elaine_bf <- FALSE
+all.text$kramer_gf <- FALSE
+for(y in seq_along(all.text$episode)){
+    h <- episodes[episodes$title %in% all.text$episode[y], ]
+    all.text$season[y] <- h$season
+    all.text$writers[y] <- h$writers
+    all.text$Jerry[y] <- h$Jerry
+    all.text$Larry[y] <- h$Larry
+    if(all.text$speaker[y] %in% girls) all.text$jerry_gf[y] <- TRUE
+    if(all.text$speaker[y] %in% georges_girls) all.text$george_gf[y] <- TRUE
+    if(all.text$speaker[y] %in% elaines_bfs) all.text$elaine_bf[y] <- TRUE
+    if(all.text$speaker[y] %in% kramers_girls) all.text$kramer_gf[y] <- TRUE
+    all.text$gender[y] <- gender[gender$Name %in% all.text$speaker[y], 3]
+}
+all.text <- all.text[,c(2,1,15,4,6,11,3,5,12,7:10,13,14)]
+#write.csv(all.text, "seinfeld_scripts_enhanced.csv", row.names=F)
+
+
+
+
